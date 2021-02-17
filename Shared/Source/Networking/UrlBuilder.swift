744cec5febd2afa7ef924fdaf436f3dd9da155df
佐藤悠真 <archyusa@icloud.com>
2021-02-17 23:56:36 +0000

    Added URL builder for future network requests

---
+++ Shared/Source/Networking/UrlBuilder.swift
@@ -0,0 +1,45 @@
+//
+// The LanguagePractice project.
+// Created by optionaldev on 17/02/2021.
+// Copyright © 2021 optionaldev. All rights reserved.
+// 
+
+
+import Foundation
+
+extension Language {
+    
+    var url: URL? {
+        return UrlBuilder.jsonUrl(forName: rawValue)
+    }
+}
+
+final class UrlBuilder {
+    
+    static func imageUrl(forID imageID: String) -> URL? {
+        var components = baseGithubUrlComponents
+        
+        components.path.append("images/" + imageID + ".jpg")
+        
+        return components.url
+    }
+    
+    // MARK: - Private
+    
+    private static let baseGithubUrlComponents: URLComponents = {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "raw.githubusercontent.com"
+        components.path = "/optionaldev/LanguagePracticeTestData/main/"
+        
+        return components
+    }()
+    
+    fileprivate static func jsonUrl(forName name: String) -> URL? {
+        var components = baseGithubUrlComponents
+        
+        components.path.append("jsons/" + name + ".json")
+        
+        return components.url
+    }
+}
