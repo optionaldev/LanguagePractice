33cadea0724aaa1b7230ae446abd7735d91ed9af
佐藤悠真 <archyusa@icloud.com>
2021-03-23 23:35:06 +0000

    Added ImageCache to avoid having to read images off disk during challenge

---
+++ Shared/Source/Utility/ImageCache.swift
@@ -0,0 +1,37 @@
+//
+// The LanguagePractice project.
+// Created by optionaldev on 23/03/2021.
+// Copyright © 2021 optionaldev. All rights reserved.
+// 
+
+#if os(iOS)
+import class UIKit.UIImage
+typealias CustomImage = UIImage
+#else
+import AppKit
+typealias CustomImage = NSImage
+#endif
+
+final class ImageCache {
+    
+    var images: [String: CustomImage] = [:]
+    
+    func image(forID id: String) -> CustomImage? {
+        if let cachedImage = images[id] {
+            log("returning cached image", type: .info)
+            return cachedImage
+        }
+        if let imageData = Persistence.imageData(forID: id) {
+            guard let uiImage = CustomImage(data: imageData) else {
+                log("Unable to create UIImage from path: \"\(Persistence.imagePath(id: id) ?? "<unknown>") \"", type: .unexpected)
+                return nil
+            }
+            images[id] = uiImage
+            log("returning normal image", type: .info)
+            return uiImage
+        }
+        
+        log("Path doesn't exist, but we shouldn't have created a challenge with image type", type: .unexpected)
+        return nil
+    }
+}
