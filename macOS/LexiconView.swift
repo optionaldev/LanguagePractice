1aa9a512d6524ff6c30086ab689d0cd93dc07d23
佐藤悠真 <archyusa@icloud.com>
2022-05-20 20:28:23 +0300

    Added detail views for lexicon words

--- macOS/LexiconView.swift
+++ macOS/LexiconView.swift
@@ -8,6 +8,8 @@ import protocol SwiftUI.View
 
 import struct SwiftUI.HStack
 import struct SwiftUI.List
+import struct SwiftUI.NavigationLink
+import struct SwiftUI.NavigationView
 import struct SwiftUI.ObservedObject
 import struct SwiftUI.Picker
 import struct SwiftUI.SegmentedPickerStyle
@@ -16,26 +18,27 @@ import struct SwiftUI.Text
 import struct SwiftUI.TextField
 import struct SwiftUI.VStack
 
-
 struct LexiconView: View {
  
   var body: some View {
-    HStack {
-      VStack {
-        Picker("", selection: $viewModel.selection) {
-          // The picker works by going through the tags
-          Text("English").tag(Language.english)
-          Text("Foreign").tag(Language.foreign)
-        }.pickerStyle(SegmentedPickerStyle())
-        TextField("", text: $viewModel.searchString)
-          .frame(width: 150, height: 40)
-          .multilineTextAlignment(.center)
-        List(viewModel.displayedItems, id: \.id) { item in
-          Text(item.id)
+    NavigationView {
+      HStack {
+        VStack {
+          Picker("", selection: $viewModel.selection) {
+            // The picker works by going through the tags
+            Text("English").tag(Language.english)
+            Text("Foreign").tag(Language.foreign)
+          }.pickerStyle(SegmentedPickerStyle())
+          TextField("", text: $viewModel.searchString)
+            .frame(width: 150, height: 40)
+            .multilineTextAlignment(.center)
+          List(viewModel.displayedItems, id: \.id) { item in
+            NavigationLink(item.id, destination: NavigationLazyView(LexiconDetailsView(item: item, language: viewModel.selection)))
+          }
         }
+        .frame(width: 150)
+        Spacer()
       }
-      .frame(width: 150)
-      Spacer()
     }
   }
   
